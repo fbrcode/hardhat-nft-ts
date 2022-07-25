030f760220bedeeca7467fde3387155d2ccc2ff3
fbrcode <fabio.bressler@gmail.com>
2022-07-25 11:29:34 -0300

    log deploy and single object return

--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -4,6 +4,10 @@ const path = require('path');
 const fs = require('fs');
 require('dotenv').config();
 
+// use log instead of console.log
+const { deployments } = require('hardhat');
+const { log } = deployments;
+
 const pinataApiKey = process.env.PINATA_API_KEY;
 const pinataApiSecret = process.env.PINATA_API_SECRET;
 const pinata = pinataSdk(pinataApiKey, pinataApiSecret);
@@ -11,20 +15,26 @@ const pinata = pinataSdk(pinataApiKey, pinataApiSecret);
 async function storeImages(imagesFilePath) {
   const fullImagesPath = path.resolve(__dirname, imagesFilePath);
   const files = fs.readdirSync(fullImagesPath);
-  // console.log(`Found ${files} images`);
+  const pngFiles = files.filter((f) => f.endsWith('.png'));
+  // log(`Found ${JSON.stringify(pngFiles, null, 2)} images`);
   let responses = [];
-  console.log('Uploading images to Pinata IPFS...');
-  for (const file of files) {
-    console.log(`Uploading ${file}...`);
+  log('Uploading images to Pinata IPFS...');
+  for (const file of pngFiles) {
+    // log(`Uploading ${file}...`);
     const readableStreamForFile = fs.createReadStream(`${fullImagesPath}/${file}`);
     try {
       const response = await pinata.pinFileToIPFS(readableStreamForFile);
-      responses.push(response);
+      log(`Uploaded ${file} to Pinata IPFS: ${response.IpfsHash}`);
+      responses.push({ ...response, fileName: file });
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      throw error;
     }
   }
-  return { responses, files };
+  log('All images uploaded to Pinata IPFS!');
+  // log(`Responses: ${JSON.stringify(responses, null, 2)}`);
+  // log(`Files: ${JSON.stringify(pngFiles, null, 2)}`);
+  return responses;
 }
 
 async function storeTokenUriMetadata(metadata) {
@@ -32,9 +42,9 @@ async function storeTokenUriMetadata(metadata) {
     const response = await pinata.pinJSONToIPFS(metadata);
     return response;
   } catch (error) {
-    console.log(error);
+    // console.log(error);
+    throw error;
   }
-  return null;
 }
 
 module.exports = {
