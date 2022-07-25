10f886e0f4199c6cab7eeea2cdd60c10c91129a2
fbrcode <fabio.bressler@gmail.com>
2022-07-25 07:22:35 -0300

    deploy vrf mock

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,23 @@
+const { network } = require('hardhat');
+
+const MOCK_BASE_FEE = '250000000000000000'; // 0.25 is this the premium in LINK?
+const MOCK_GAS_PRICE_LINK = 1e9; // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  if (!network.live) {
+    log(`Deploying mocks on network: ${network.name}`);
+    // deploy a mock vrf coordinator
+    await deploy('VRFCoordinatorV2Mock', {
+      from: deployer,
+      args: [MOCK_BASE_FEE, MOCK_GAS_PRICE_LINK],
+      log: true,
+    });
+    log(`Mocks Deployed!`);
+    log(`----------------------------------------------------------`);
+  }
+};
+
+module.exports.tags = ['all', 'mocks'];
